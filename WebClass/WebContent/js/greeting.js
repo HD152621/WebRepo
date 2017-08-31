b718d9aa35d68cbae36b724d535db361943f1a5b
HD152621 <ymh3693@naver.com>
2017-08-31 14:01:35 +0900

    최초 생성

---
+++ WebClass/WebContent/js/greeting.js
@@ -0,0 +1,10 @@
+function greeting(nation){
+  if(nation == 'k')
+  document.getElementById('result').innerHTML = "안녕하세요";
+  else if(nation == 'e'){
+      document.getElementById('result').innerHTML = "헬로우";
+  }
+  else if(nation == 'c'){
+      document.getElementById('result').innerHTML = "니하오";
+  }
+}
